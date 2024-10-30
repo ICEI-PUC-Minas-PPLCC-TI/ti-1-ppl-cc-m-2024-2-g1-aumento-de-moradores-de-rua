bdc17f9341783b3a50890c27aa79acebf2a589ae
Eduardo-Aniceto <eduardolwk@gmail.com>
2024-10-30 16:41:02 -0300

    dbjon incrementado, minhas-doacoes.html quase finalizado, Perfil.html finalizado, configuracoes.html finalizado, doacao.html finalizado, pagamento.html finalizado, script-conf finalizado, script.js finalizado, script-doacoes.js criado

--- codigo/public/sprint1/eduardo-henrique-aniceto-teixeira/js/script.js
+++ codigo/public/sprint1/eduardo-henrique-aniceto-teixeira/js/script.js
@@ -1,6 +1,6 @@
 var usuarioCorrente = {};
 
-function carregarPerfis() {
+function carregarPerfil() {
     fetch('/usuarios') // Insira o caminho correto do arquivo JSON
         .then(response => response.json())
         .then(data => {
@@ -13,16 +13,22 @@ function carregarPerfis() {
                 console.log("Perfil: " + perfil);
 
                 if (perfil) {
+
+                    
                     // Atualizar o conteúdo do HTML com os dados do perfil
                     document.getElementById('nome').textContent = perfil.nome || "Nome não disponível";
+                    document.getElementById('data_nascimento').textContent = perfil.data_nascimento || "Data não disponível";
                     document.getElementById('estado').textContent = perfil.endereco.estado || "Estado não disponível";
                     document.getElementById('cidade').textContent = perfil.endereco.cidade || "Cidade não disponível";
+                    document.getElementById('bairro').textContent = perfil.endereco.bairro || "Bairro não disponível";
+                    document.getElementById('logradouro').textContent = perfil.endereco.logradouro || "Rua não disponível";
+                    document.getElementById('cep').textContent = perfil.endereco.cep || "CEP não disponível";
+                    document.getElementById('complemento').textContent = perfil.endereco.complemento || "Complemento não disponível";
                     document.getElementById('telefone').textContent = perfil.contatos.telefone || "Telefone não disponível";
                     document.getElementById('email').textContent = perfil.contatos.email || "Email não disponível";
-                    document.getElementById('info-adicional').textContent = perfil.infoAdicional || "Informação adicional não disponível";
-
+                    document.getElementById('infoAdicional').textContent = perfil.infoAdicional || "Informação adicional não disponível";
                     // Exibir foto do perfil
-                    const profilePhoto = document.getElementById('profilePhoto');
+                    const profilePhoto = document.getElementById('imgPerfil');
                     profilePhoto.innerHTML = `<img src="${perfil.imgPerfil}" alt="Foto de perfil" style="max-width: 250px; max-height: 250px;">`;
                 } else {
                     console.error('Perfil não encontrado para o ID especificado.');
@@ -40,5 +46,5 @@ window.onload = () => {
     if (usuarioCorrenteJSON) {
         usuarioCorrente = JSON.parse(usuarioCorrenteJSON);
     }
-    carregarPerfis();
+    carregarPerfil();
 };
