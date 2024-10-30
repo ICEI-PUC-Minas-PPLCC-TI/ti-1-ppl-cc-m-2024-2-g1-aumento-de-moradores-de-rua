39a6365384ee353dfd4259b924aab6cceb636efd
vloax <vitords.mello@gmail.com>
2024-10-30 06:09:19 -0300

    feat: add listagem de atividades

--- codigo/public/assets/js/home/index.js
+++ codigo/public/assets/js/home/index.js
@@ -1,10 +1,24 @@
-// JavaScript
+let currentUser = {};
+
+const formatDate = (dateStr) => {
+  if (!dateStr) return '';
+  const [year, month, day] = dateStr.split("-");
+  return `${day}/${month}/${year}`;
+}
+
 const fetchData = () => {
+  currentUser = JSON.parse(sessionStorage.getItem('usuarioCorrente')) || {};
+
   fetch("/ongs")
     .then((res) => res.json())
     .then((ongs) => {
       const ongsList = document.querySelector(".ongs-list");
       ongsList.innerHTML = "";
+
+      if (ongs.length === 0) {
+        ongsList.innerHTML = "<p>Nenhum registro encontrado.</p>";
+        return;
+      }
 
       ongs.forEach((ong) => {
         const ongElement = `
@@ -20,14 +34,50 @@ const fetchData = () => {
       });
     })
     .catch((error) => {
-      console.error('Erro ao buscar os dados:', error);
+      console.error('Erro ao buscar os dados das ONGs:', error);
+      const ongsList = document.querySelector(".ongs-list");
+      ongsList.innerHTML = "<p>Erro ao carregar os registros das ONGs.</p>";
+    });
+    
+  console.log('currentUser:', currentUser);
+
+  fetch(`/atividades?voluntario=${currentUser.id}`)
+    .then((res) => res.json())
+    .then((atividades) => {
+      const atividadesList = document.querySelector(".atividades-list");
+      atividadesList.innerHTML = "";
+
+      if (atividades.length === 0) {
+        atividadesList.innerHTML = "<p>Nenhum registro encontrado.</p>";
+        return;
+      }
+
+      atividades.forEach((atividade) => {
+        const atividadeElement = `
+          <div class="card">
+            <img class="card_image" src="${atividade.imageUrl || './assets/images/home_img.jpg'}" alt="${atividade.nome_fantasia}">
+            <p>${atividade.nome}</p>
+            <p>Data Início: ${formatDate(atividade.data_inicio)}</p>
+            <p>Data Fim: ${formatDate(atividade.data_fim)}</p>
+            <button onclick="verDetalhesAtividade(${atividade.id})">Ver Detalhes</button>
+          </div>`;
+
+        atividadesList.innerHTML += atividadeElement;
+      });
+    })
+    .catch((error) => {
+      console.error('Erro ao buscar os dados das atividades:', error);
+      const atividadesList = document.querySelector(".atividades-list");
+      atividadesList.innerHTML = "<p>Erro ao carregar os registros das atividades.</p>";
     });
 };
 
-// Função para manipular o clique no botão de detalhes
 const verDetalhes = (id) => {
   console.log('Ver detalhes da ONG com ID:', id);
 };
 
-// Chamar a função para buscar e renderizar os dados
+const verDetalhesAtividade = (id) => {
+  console.log('Ver detalhes da Atividade com ID:', id);
+};
+
 fetchData();
